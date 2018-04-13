57c33e1a3ae859cb27c583fdd9d056b8b18f9cf9
luo.wenjie@linkingmed.com <luo.wenjie@linkingmed.com>
2018-04-13 18:35:12 +0800

    format

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,64 +3,66 @@
  */
 
 
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import { turnModal } from "../redux/actions/home/actions"
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import {turnModal} from "../redux/actions/home/actions"
 
-import { Button,Modal } from 'antd';
+import {Button, Modal} from 'antd';
 
 // import {}
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {}
     }
-  }
-  handleButtonClick(){
-    this.props.actions.turnModal(true)
-  }
-  handleOk(){
-    console.log("ok")
-  }
-  handleCancel(){
-    this.props.actions.turnModal(false)
-  }
-  render() {
-    const {visible}  = this.props;
 
-    console.log('render')
-    return (
-      <div className="pages-home">
-        <Button type="primary" onClick={this.handleButtonClick.bind(this)}>Primary</Button>
-        <Modal
-          title="Basic Modal"
-          visible={visible}
-          onOk={this.handleOk.bind(this)}
-          onCancel={this.handleCancel.bind(this)}
-        >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-        </Modal>
-      </div>
-    )
-  }
+    handleButtonClick() {
+        this.props.actions.turnModal(true)
+    }
+
+    handleOk() {
+        console.log("ok")
+    }
+
+    handleCancel() {
+        this.props.actions.turnModal(false)
+    }
+
+    render() {
+        const {visible} = this.props;
+
+        console.log('render')
+        return (
+            <div className="pages-home">
+                <Button type="primary" onClick={this.handleButtonClick.bind(this)}>Primary</Button>
+                <Modal
+                    title="Basic Modal"
+                    visible={visible}
+                    onOk={this.handleOk.bind(this)}
+                    onCancel={this.handleCancel.bind(this)}
+                >
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
+            </div>
+        )
+    }
 }
 
 // 将 store 中的数据作为 props 绑定到 Home
 const mapStateToProps = (state, ownProps) => {
-  let { Test } = state;
-  return {
-    visible: Test.visible
-  }
+    let {Test} = state;
+    return {
+        visible: Test.visible
+    }
 };
 
 // 将 action 作为 props 绑定到 Home
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  actions: bindActionCreators({ turnModal }, dispatch)
+    actions: bindActionCreators({turnModal}, dispatch)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
